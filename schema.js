5594953af86cf78d40f56c9e265e08e0f3e1e9b0
Adesh <adesh02092000@gmail.com>
2023-10-08 18:23:52 +0530

    Added resolvers for update Mutation

--- schema.js
+++ schema.js
@@ -34,6 +34,7 @@ export const typeDefs = `#graphql
 
     type Mutation {
         addGame(game: AddGameInput!): Game
+        updateGame(id: ID!, edits: EditGameInput!): Game
         deleteGame(id: ID!): [Game]
     }
 
@@ -41,5 +42,9 @@ export const typeDefs = `#graphql
     input AddGameInput {
         title: String!
         platform: [String!]!
+    }
+    input EditGameInput {
+        title: String
+        platform: [String!]
     }
 `
