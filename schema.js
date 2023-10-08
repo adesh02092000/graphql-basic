178b5c53c5334a2ddbaced48f6bae9b7c4e5563e
Adesh <adesh02092000@gmail.com>
2023-10-08 14:07:47 +0530

    added query variables for individual resources

--- schema.js
+++ schema.js
@@ -21,7 +21,10 @@ export const typeDefs = `#graphql
     # Query entry points
     type Query {
         reviews: [Review]
+        review(id: ID!): Review
         games: [Game]
+        game(id: ID!): Game
         authors: [Author]
+        author(id: ID!): Author
     }
 `
